a6d82e3cc81f682aaa187d87af0d81498f4d5e4a
Daniel Tridico <mail@danieltridico.com>
2019-02-13 14:46:05 +0200

    another push

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-  validates :description, uniqueness: true, presence: true
-  validates :cocktail, :ingredient, presence: true
+  validates :description, presence: true
+  validates :cocktail_id, uniqueness: { scope: :ingredient_id }
 end
